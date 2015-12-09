45a26ed3a2e40e10d9fe18ed6dd1966a8b993af8
Yannick Lin <yannicklin@twoudia.com>
2015-12-09 21:52:01 +1000

    Make work and icon/splash updated

--- APP-WordPress-API/APP-WordPress-API/platforms/android/assets/www/js/app.js
+++ APP-WordPress-API/APP-WordPress-API/platforms/android/assets/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.filters', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -76,6 +76,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           }
       }
   })
+      .state('tab.posts-cht', {
+          url: '/posts-cht',
+          views: {
+              'tab-posts-cht': {
+                  templateUrl: 'templates/tab-posts-cht.html',
+                  controller: 'Posts-CHTCtrl'
+              }
+          }
+      })
+
     .state('tab.post-detail', {
         url: '/posts/:postId',
         views: {
--- APP-WordPress-API/APP-WordPress-API/platforms/android/assets/www/js/controllers.js
+++ APP-WordPress-API/APP-WordPress-API/platforms/android/assets/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function ($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
@@ -13,20 +13,167 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('PostsCtrl', function ($scope, $http) {
+.controller('PostsCtrl', function ($scope, $http, DataLoader, $timeout, $log, $ionicLoading) {
 
-    $http.get(DOMAIN_POST_URI)
-        .success(function (response) {
-            $scope.posts = response;
+    var dataURI = 'http://chineselearn.info/wp-json/wp/v2/';
+
+    var postsAPI = dataURI + 'posts';
+
+    $scope.moreItems = false;
+
+    $scope.loadPosts = function() {
+
+        // Get all of our posts
+        DataLoader.get( postsAPI ).then(function(response) {
+
+            $scope.posts = response.data;
+
+            $scope.moreItems = true;
+
+            $log.log(postsAPI, response.data);
+            $ionicLoading.hide();
+        }, function(response) {
+            $log.log(postsAPI, response.data);
+            $ionicLoading.hide();
         });
+
+    }
+
+    // Load posts on page load
+    $scope.loadPosts();
+
+    paged = 2;
+
+    // Load more (infinite scroll)
+    $scope.loadMore = function() {
+
+        if( !$scope.moreItems ) {
+            return;
+        }
+
+        var pg = paged++;
+
+        $log.log('loadMore ' + pg );
+
+        $timeout(function() {
+
+            DataLoader.get( postsAPI + '?page=' + pg ).then(function(response) {
+
+                angular.forEach( response.data, function( value, key ) {
+                    $scope.posts.push(value);
+                });
+
+                if( response.data.length <= 0 ) {
+                    $scope.moreItems = false;
+                }
+            }, function(response) {
+                $scope.moreItems = false;
+                $log.error(response);
+            });
+
+
+        }, 1000);
+
+    }
+
+    $scope.moreDataExists = function() {
+        return $scope.moreItems;
+    }
+
+    // Pull to refresh
+    $scope.doRefresh = function() {
+  
+        $timeout( function() {
+
+            $scope.loadPosts();
+
+        }, 1000);
+      
+    };
+    
 })
 
-.controller('PostDetailCtrl', function ($scope, $stateParams, Posts) {
+    .controller('Posts-CHTCtrl', function ($scope, $http, DataLoader, $timeout, $log, $ionicLoading) {
 
-    $http.get(DOMAIN_POST_URI + "/" + $stateParams.postId)
-            .success(function (response) {
-                $scope.post = response;
+        var dataURI = 'http://chineselearn.info/zh-hant/wp-json/wp/v2/';
+
+        var postsAPI = dataURI + 'posts';
+
+        $scope.loadPosts = function () {
+
+            // Get all of our posts
+            DataLoader.get(postsAPI).then(function (response) {
+
+                $scope.posts = response.data;
+
+                $scope.moreItems = true;
+
+                $log.log(postsAPI, response.data);
+                $ionicLoading.hide();
+            }, function (response) {
+                $log.log(postsAPI, response.data);
+                $ionicLoading.hide();
             });
+
+        }
+
+        // Load posts on page load
+        $scope.loadPosts();
+
+
+        // Pull to refresh
+        $scope.doRefresh = function () {
+
+            $timeout(function () {
+
+                $scope.loadPosts();
+
+            }, 1000);
+
+        };
+
+    })
+
+.controller('PostDetailCtrl', function ($scope, $stateParams, $http, DataLoader, $sce, $timeout, $log, $ionicLoading) {
+
+    $scope.itemID = $stateParams.postId;
+
+    var dataURI = 'http://chineselearn.info/wp-json/wp/v2/';
+
+    var singlePostApi = dataURI + 'posts/' + $scope.itemID;
+
+    $scope.loadPost = function() {
+
+        DataLoader.get( singlePostApi ).then(function(response) {
+
+            $scope.post = response.data;
+
+            $log.debug($scope.post);
+
+            // Don't strip post html
+            $scope.content = $sce.trustAsHtml(response.data.content.rendered);
+
+            $ionicLoading.hide();
+        }, function(response) {
+            $log.error('error', response);
+            $ionicLoading.hide();
+        });
+
+    }
+
+    $scope.loadPost();
+
+    // Pull to refresh
+    $scope.doRefresh = function() {
+  
+        $timeout( function() {
+
+            $scope.loadPost();
+    
+        }, 1000);
+      
+    };
+
 })
 
 .controller('AccountCtrl', function($scope) {
