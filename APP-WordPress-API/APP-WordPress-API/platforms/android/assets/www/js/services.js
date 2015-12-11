8bff2cbf0a8cfca9d7c4303adf09c6309999a6e7
Yannick Lin <yannicklin@twoudia.com>
2015-12-12 01:57:07 +1000

    translate works and also embed cookies for further dev.

--- APP-WordPress-API/APP-WordPress-API/platforms/android/assets/www/js/services.js
+++ APP-WordPress-API/APP-WordPress-API/platforms/android/assets/www/js/services.js
@@ -1,10 +1,11 @@
-angular.module('starter.services', [])
+angular.module('chineselearn.services', [])
 
-.factory('DataLoader', function ($http) {
+.factory('DataLoader', function ($http, $log) {
     var $domainURI = 'http://chineselearn.info/';
     return {
         get: function ($lang, $term) {
             var $url = $domainURI + $lang + 'wp-json/wp/v2/' + $term;
+            $log.debug($url);
             return $http.get($url);
         }
     }
