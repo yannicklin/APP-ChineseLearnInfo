1fa793f5ddc8664cbc2987c0f2a7ceb34724c56d
Yannick Lin <yannicklin.web@gmail.com>
2020-09-18 21:10:35 +1000

    add some modules (react-native-pdf, patch-package, etc.)

---
+++ i18n.js
@@ -0,0 +1,38 @@
+import {I18nManager} from 'react-native';
+import i18n from 'i18n-js';
+import * as RNLocalize from 'react-native-localize';
+import memoize from 'lodash.memoize';
+
+export const translationGetters = {
+  // lazy requires (metro bundler does not support symlinks)
+  de: () => require('./assets/i18n/de.json'),
+  en: () => require('./assets/i18n/en.json'),
+  es: () => require('./assets/i18n/es.json'),
+  fr: () => require('./assets/i18n/fr.json'),
+  ja: () => require('./assets/i18n/ja.json'),
+  pt: () => require('./assets/i18n/pt.json'),
+  ru: () => require('./assets/i18n/ru.json'),
+  zh: () => require('./assets/i18n/zh.json'),
+};
+
+export const i18nT = memoize(
+  (key, config) =>
+    i18n.t(key, config).includes('missing') ? key : i18n.t(key, config),
+  (key, config) => (config ? key + JSON.stringify(config) : key),
+);
+
+export const setI18nConfig = () => {
+  const fallback = {languageTag: 'en', isRTL: false};
+  const {languageTag, isRTL} =
+    RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) ||
+    fallback;
+
+  // clear translation cache
+  translate.cache.clear();
+  // update layout direction
+  I18nManager.forceRTL(isRTL);
+
+  // set i18n-js config
+  i18n.translations = {[languageTag]: translationGetters[languageTag]()};
+  i18n.locale = languageTag;
+};
--- tabs/tabDash.tsx
+++ tabs/tabDash.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {Header, Card} from 'react-native-elements';
-import I18n from '../i18n';
+import {i18nT} from '../i18n';
 
 export default function TabDash() {
   return (
@@ -9,56 +9,56 @@ export default function TabDash() {
       <Header
         leftComponent={{icon: 'menu', color: '#fff'}}
         centerComponent={{
-          text: I18n.t('DASH_NEWS_TITLE_20170816'),
+          text: i18nT('DASH_NEWS_TITLE_20170816'),
           style: {color: '#fff'},
         }}
         rightComponent={{icon: 'home', color: '#fff'}}
       />
       <Card>
-        <Card.Title>{I18n.t('DASH_NEWS_TITLE_20170816')}</Card.Title>
+        <Card.Title>{i18nT('DASH_NEWS_TITLE_20170816')}</Card.Title>
         <Card.Divider />
         <Text style={{marginBottom: 10}}>
-          {I18n.t('DASH_NEWS_CONTENT_20170816')}
+          {i18nT('DASH_NEWS_CONTENT_20170816')}
         </Text>
       </Card>
 
       <Card>
-        <Card.Title>{I18n.t('DASH_NEWS_TITLE_20170718')}</Card.Title>
+        <Card.Title>{i18nT('DASH_NEWS_TITLE_20170718')}</Card.Title>
         <Card.Divider />
         <Text style={{marginBottom: 10}}>
-          {I18n.t('DASH_NEWS_CONTENT_20170718')}
+          {i18nT('DASH_NEWS_CONTENT_20170718')}
         </Text>
       </Card>
 
       <Card>
-        <Card.Title>{I18n.t('DASH_NEWS_TITLE_20160304')}</Card.Title>
+        <Card.Title>{i18nT('DASH_NEWS_TITLE_20160304')}</Card.Title>
         <Card.Divider />
         <Text style={{marginBottom: 10}}>
-          {I18n.t('DASH_NEWS_CONTENT_20160304')}
+          {i18nT('DASH_NEWS_CONTENT_20160304')}
         </Text>
       </Card>
 
       <Card>
-        <Card.Title>{I18n.t('DASH_NEWS_TITLE_20160229')}</Card.Title>
+        <Card.Title>{i18nT('DASH_NEWS_TITLE_20160229')}</Card.Title>
         <Card.Divider />
         <Text style={{marginBottom: 10}}>
-          {I18n.t('DASH_NEWS_CONTENT_201602229')}
+          {i18nT('DASH_NEWS_CONTENT_201602229')}
         </Text>
       </Card>
 
       <Card>
-        <Card.Title>{I18n.t('DASH_NEWS_TITLE_20160228')}</Card.Title>
+        <Card.Title>{i18nT('DASH_NEWS_TITLE_20160228')}</Card.Title>
         <Card.Divider />
         <Text style={{marginBottom: 10}}>
-          {I18n.t('DASH_NEWS_CONTENT_20160228')}
+          {i18nT('DASH_NEWS_CONTENT_20160228')}
         </Text>
       </Card>
 
       <Card>
-        <Card.Title>{I18n.t('DASH_NEWS_TITLE_20160227')}</Card.Title>
+        <Card.Title>{i18nT('DASH_NEWS_TITLE_20160227')}</Card.Title>
         <Card.Divider />
         <Text style={{marginBottom: 10}}>
-          {I18n.t('DASH_NEWS_CONTENT_20160227')}
+          {i18nT('DASH_NEWS_CONTENT_20160227')}
         </Text>
       </Card>
     </View>
